f84c9ad74e64d444848333b0c97e445d8fbc4094
pandusanika <pandu.martinez24@gmail.com>
2022-12-28 16:04:20 +0700

    fix overflow comment.dart

--- lib/screen/comment.dart
+++ lib/screen/comment.dart
@@ -47,105 +47,110 @@ class _CommentState extends State<Comment> {
     if (_m == null) {
       return const CircularProgressIndicator();
     }
-    return Card(
-        child: Container(
-            height: 500,
-            width: 400,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Container(
-                  width: 400,
-                  height: 400,
-                  child: Stack(
-                    children: [
-                      Container(
-                        height: 400,
-                        width: 400,
-                        decoration: BoxDecoration(
-                            image: DecorationImage(
-                                image: NetworkImage(_m!.url_image),
-                                fit: BoxFit.cover)),
-                      ),
-                      Container(
-                        padding: EdgeInsets.symmetric(vertical: 10),
-                        height: 400,
-                        width: 400,
-                        alignment: Alignment.bottomCenter,
-                        child: Text(
-                          _m!.bottom_text,
-                          style: TextStyle(
-                            fontSize: 40,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.symmetric(vertical: 10),
-                        height: 400,
-                        width: 400,
-                        alignment: Alignment.topCenter,
-                        child: Text(
-                          _m!.top_text,
-                          style: TextStyle(
-                            fontSize: 40,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  width: 400,
-                  height: 50,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
+    return SingleChildScrollView(
+        child: Column(
+      children: <Widget>[
+        Card(
+            child: Container(
+                height: 475,
+                width: 400,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Container(
+                      width: 400,
+                      height: 400,
+                      child: Stack(
                         children: [
-                          Padding(
-                              padding: EdgeInsets.all(5),
-                              child: IconButton(
-                                  onPressed: () {},
-                                  icon: Icon(
-                                    Icons.favorite,
-                                    color: Colors.red,
-                                  ))),
-                          Text(_m!.number_likes.toString() + " likes"),
+                          Container(
+                            height: 400,
+                            width: 400,
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image: NetworkImage(_m!.url_image),
+                                    fit: BoxFit.cover)),
+                          ),
+                          Container(
+                            padding: EdgeInsets.symmetric(vertical: 10),
+                            height: 400,
+                            width: 400,
+                            alignment: Alignment.bottomCenter,
+                            child: Text(
+                              _m!.bottom_text,
+                              style: TextStyle(
+                                fontSize: 40,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.symmetric(vertical: 10),
+                            height: 400,
+                            width: 400,
+                            alignment: Alignment.topCenter,
+                            child: Text(
+                              _m!.top_text,
+                              style: TextStyle(
+                                fontSize: 40,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
                         ],
                       ),
-                    ],
-                  ),
-                ),
-                Divider(
-                  height: 20,
-                ),
-                Card(
-                    elevation: 10,
-                    margin: const EdgeInsets.all(5),
-                    child: Column(
-                      children: <Widget>[
-                        Padding(
-                            padding: const EdgeInsets.all(5),
-                            child: ListView.builder(
-                                shrinkWrap: true,
-                                itemCount: _m?.users?.length,
-                                itemBuilder: (BuildContext ctxt, int index) {
-                                  return ListTile(
-                                    title: Text(_m?.users?[index]['username']),
-                                    subtitle: Text(
-                                        _m?.users?[index]['comment_content']),
-                                  );
-                                })),
-                      ],
-                    ))
+                    ),
+                    Container(
+                      width: 400,
+                      height: 75,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Padding(
+                                  padding: EdgeInsets.all(5),
+                                  child: IconButton(
+                                      onPressed: () {},
+                                      icon: Icon(
+                                        Icons.favorite,
+                                        color: Colors.red,
+                                      ))),
+                              Text(_m!.number_likes.toString() + " likes"),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ))),
+        Divider(
+          height: 20,
+        ),
+        Card(
+            elevation: 10,
+            margin: const EdgeInsets.all(5),
+            child: Column(
+              children: <Widget>[
+                Padding(
+                    padding: const EdgeInsets.all(5),
+                    child: ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: _m?.users?.length,
+                        itemBuilder: (BuildContext ctxt, int index) {
+                          return ListTile(
+                            title: Text(_m?.users?[index]['username']),
+                            subtitle:
+                                Text(_m?.users?[index]['comment_content']),
+                          );
+                        })),
               ],
-            )));
+            ))
+      ],
+    ));
   }
 
   @override
